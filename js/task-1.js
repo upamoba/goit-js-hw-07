92724290d717d35cbe48bd489c68032009b6e093
uty <fgmapam@gmail.com>
2025-04-15 20:21:22 +0300

    summary(required)

---
+++ js/task-1.js
@@ -0,0 +1,9 @@
+const categories = document.querySelectorAll('#categories .item');
+console.log(`Number of categories: ${categories.length}`);
+
+categories.forEach(category => {
+  const categoryName = category.querySelector('h2').textContent;
+  const elementsCount = category.querySelectorAll('ul li').length;
+  console.log(`Category: ${categoryName}`);
+  console.log(`Elements: ${elementsCount}`);
+});
